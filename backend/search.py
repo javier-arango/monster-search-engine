cd38bbeb3f4cc7d9d12dcc3bd1992f6388c39714
Akshay Sharma <42249933+akshaysharmajs@users.noreply.github.com>
2022-09-10 18:23:56 -0400

    Initialize DB func

--- backend/search.py
+++ backend/search.py
@@ -1,47 +1,42 @@
 import pandas as pd
 import sqlite3
 
-# def isempty(conn):
-#     c = conn.cursor()
-
-#     full = 0
-#     exist = c.fetchone()
-
-#     if exist is None:
-#         return True
-    
-#     return False
+def isempty(conn):
+    c = conn.execute("select exists (select 1 from securities);")
+    if c.fetchone()[0]:
+        return False
+    return True
     
 
-# def initialize_db(df, conn):
-#     conn.execute("""CREATE TABLE IF NOT EXISTS Securities (
-#                     "security_id" VARCHAR,
-#                     "root_symbol" VARCHAR, 
-#                     "bbg" VARCHAR, 
-#                     "symbol" VARCHAR, 
-#                     "ric" VARCHAR, 
-#                     "cusip" VARCHAR, 
-#                     "isin" VARCHAR, 
-#                     "bb_yellow" VARCHAR, 
-#                     "bloomberg" VARCHAR, 
-#                     "spn" VARCHAR, 
-#                     "sedol" VARCHAR
-#                     );""")
+def initialize_db(df, conn):
+    conn.execute("""CREATE TABLE IF NOT EXISTS Securities (
+                    "security_id" VARCHAR,
+                    "root_symbol" VARCHAR, 
+                    "bbg" VARCHAR, 
+                    "symbol" VARCHAR, 
+                    "ric" VARCHAR, 
+                    "cusip" VARCHAR, 
+                    "isin" VARCHAR, 
+                    "bb_yellow" VARCHAR, 
+                    "bloomberg" VARCHAR, 
+                    "spn" VARCHAR, 
+                    "sedol" VARCHAR
+                    );""")
     
-#     if isempty(conn):
-#         for i in range(len(df)):
-#             sql = f"""INSERT INTO Securities VALUES ('{str(df.at[i, 'security_id'])}',
-#                                                 '{str(df.at[i, 'root_symbol'])}',
-#                                                 '{str(df.at[i, 'bbg'])}',
-#                                                 '{str(df.at[i, 'symbol'])}',
-#                                                 '{str(df.at[i, 'ric'])}',
-#                                                 '{str(df.at[i, 'cusip'])}',
-#                                                 '{str(df.at[i, 'isin'])}',
-#                                                 '{str(df.at[i, 'bb_yellow'])}',
-#                                                 '{str(df.at[i, 'bloomberg'])}',
-#                                                 '{str(df.at[i, 'spn'])}',
-#                                                 '{str(df.at[i, 'sedol'])}');"""
-#             conn.execute(sql)
+    if isempty(conn):
+        for i in range(len(df)):
+            sql = f"""INSERT INTO Securities VALUES ('{str(df.at[i, 'security_id'])}',
+                                                '{str(df.at[i, 'root_symbol'])}',
+                                                '{str(df.at[i, 'bbg'])}',
+                                                '{str(df.at[i, 'symbol'])}',
+                                                '{str(df.at[i, 'ric'])}',
+                                                '{str(df.at[i, 'cusip'])}',
+                                                '{str(df.at[i, 'isin'])}',
+                                                '{str(df.at[i, 'bb_yellow'])}',
+                                                '{str(df.at[i, 'bloomberg'])}',
+                                                '{str(df.at[i, 'spn'])}',
+                                                '{str(df.at[i, 'sedol'])}');"""
+            conn.execute(sql)
 
 def search_table(conn, priority, query):
     relevant_ids = dict()
@@ -61,10 +56,14 @@ conn = sqlite3.connect('ShellhacksDB.db')
 
 df = pd.read_csv('Securities - Schonfeld ShellHacks.csv')
 
-# initialize_db(df, conn)
+initialize_db(df, conn)
+
+conn.commit()
 
 priority = {"root_symbol", "bbg", "symbol", "ric", "cusip", "isin", "bb_yellow", "bloomberg", "spn", "security_id", "sedol"}
 
-query = "HK"
+query = "ABC"
 
 print(search_table(conn, priority, query))
+
+conn.close()
