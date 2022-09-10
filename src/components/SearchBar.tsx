8d4571a949eee37a9a40a961d9e6ba9dcbc6b703
Javier Arango <javierarango.developer@gmail.com>
2022-09-10 16:02:44 -0400

    Add new compoennts

---
+++ src/components/SearchBar.tsx
@@ -0,0 +1,69 @@
+import React, { useState } from "react";
+import { Button, Container, TextField, Box } from "@mui/material";
+import { Stack } from "@mui/system";
+import SearchIcon from "@mui/icons-material/Search";
+import { ContainerSize } from "../types/helperTypes";
+
+import "../styles/SearchBar.css";
+import { SearchFilters } from "./SearchFilters";
+
+export const SearchBar = (props: ContainerSize) => {
+  const [userInput, setUserInput] = useState("");
+
+  return (
+    <>
+      <Container maxWidth={props.maxWidth}>
+        <Stack spacing={2}>
+          <Stack direction="row" spacing={2}>
+            {/* Search Bar */}
+            <Box
+              component={TextField}
+              boxShadow={2}
+              id="search-bar-input"
+              label={
+                <>
+                  <Stack direction="row" spacing={1}>
+                    <SearchIcon />
+                    <p>Search Value</p>
+                  </Stack>
+                </>
+              }
+              fullWidth={true}
+              value={userInput}
+              onChange={(event) => setUserInput(event.target.value)}
+            />
+
+            {/* Search Button */}
+            <Button
+              variant="contained"
+              onClick={() => {
+                // Send request to the backend
+                console.log(userInput);
+
+                // Reset text field value
+                setUserInput("");
+              }}
+            >
+              Search
+            </Button>
+          </Stack>
+
+          {/* Selectors Filters */}
+          <Stack direction={"row"} spacing={2} sx={{ width: "70%" }}>
+            <SearchFilters
+              labelId="priority-selector-label"
+              selectorId="priority-selector-label"
+              value="hello"
+            />
+
+            <SearchFilters
+              labelId="priority-selector-label"
+              selectorId="priority-selector-label"
+              value="hello"
+            />
+          </Stack>
+        </Stack>
+      </Container>
+    </>
+  );
+};
---
+++ src/types/helperTypes.ts
@@ -0,0 +1,9 @@
+export type ContainerSize = {
+  maxWidth?: "xs" | "sm" | "md" | "lg" | "xl" | false;
+};
+
+export type SelectorPrompt = {
+  labelId: String;
+  selectorId: String;
+  value: String | Number | null;
+};
